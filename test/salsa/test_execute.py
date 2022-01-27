b5d35120e23c12fae8a213815742f3647239deb2
zibaee-2021 <77154990+zibaee-2021@users.noreply.github.com>
2022-01-27 14:26:11 +0000

    Update type and format for summed_scores (from list to dict with protein id). Remove test of deleted function (`_move_longest_..`).

--- test/salsa/test_execute.py
+++ test/salsa/test_execute.py
@@ -18,6 +18,7 @@ class TestExecute(TestCase):
         self.asyn_top400 = genfromtxt(csv_asyn_top400, delimiter=',')
         csv_asyn_summed = os.path.join(abspath_root, 'test', 'salsa', 'data', 'asyn_bsc_summed.csv')
         self.asyn_summed = genfromtxt(csv_asyn_summed, delimiter=',')
+        self.asyn_id_summed_scores = {'ASYU': self.asyn_summed}
 
     def test_compute(self):
         _property = Props.bSC.value
@@ -41,38 +42,29 @@ class TestExecute(TestCase):
         summed_scores1 = np.ones((10,))
         summed_scores0 = np.zeros((20,))
         summed_scores_dict = dict()
-        summed_scores_dict['bli'] = summed_scores0
-        summed_scores_dict['blo'] = summed_scores1
+        summed_scores_dict['foo'] = summed_scores0
+        summed_scores_dict['bar'] = summed_scores1
         execute.plot_summed_scores(summed_scores_dict, _property='bla', protein_names=['ones', 'zeros'])
 
     def test_plot_summed_scores_asyn(self):
-        execute.plot_summed_scores(summed_scores=self.asyn_summed, _property=Props.bSC.value, protein_names='asyn')
+        execute.plot_summed_scores(prot_id_summed_scores=self.asyn_id_summed_scores,
+                                   _property=Props.bSC.value, protein_names='asyn')
         # This is obviously just a visual test, to be assessed by simply seeing that a
         # plot is produced without throwing an error and that it looks correct, by eye.
 
     def test__get_length_of_longest_prot_seq(self):
-        summed_scores = [np.ones(10), np.zeros(20)]
+        summed_scores = {'ones': np.ones(10), 'zeros': np.zeros(20)}
         expected = 20
         actual, _ = execute._get_length_of_longest_prot_seq(summed_scores)
         self.assertEqual(expected, actual)
 
-    def test__move_longest_seq_to_first_in_list(self):
-        summed_scores = [np.ones(10), np.zeros(20)]
-        expected = [np.zeros(20), np.ones(10)]
-        execute._move_longest_seq_to_first_in_list(summed_scores, index_of_longest=1)
-        actual = summed_scores
-        self.assertCountEqual(expected[0], actual[0])
-        npt.assert_array_equal(expected[0], actual[0])
-        self.assertCountEqual(expected[1], actual[1])
-        npt.assert_array_equal(expected[1], actual[1])
-
     def test_integrate_salsa_plot_simple(self):
-        summed_scores = np.ones((10,))
+        summed_scores = {'ones': np.ones((10,))}
         expected = 10
         actual = execute.integrate_salsa_plot(summed_scores)
         self.assertEqual(expected, actual)
 
     def test_integrate_salsa_plot_asyn(self):
         expected = 3739.2514875 # value taken from original Java implementation of salsa.
-        actual = execute.integrate_salsa_plot(self.asyn_summed)
+        actual = execute.integrate_salsa_plot(self.asyn_id_summed_scores)
         self.assertEqual(expected, actual)
