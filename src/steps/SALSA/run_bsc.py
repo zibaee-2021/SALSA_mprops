a2d9c8c46dcf891587c7b3f3288b477da7e8e8bc
zibaee-2021 <77154990+zibaee-2021@users.noreply.github.com>
2022-01-30 15:37:40 +0000

    shorten name. edit comment.

--- src/steps/SALSA/run_bsc.py
+++ src/steps/SALSA/run_bsc.py
@@ -6,21 +6,21 @@ from src.salsa import execute
 start = time.time()
 # STEP 0 - Which proteins are you interested in?
 # accession_numbers = ['']
-accession_numbers = ['P37840']
+# accession_numbers = ['P37840']
 # accession_numbers = ['P37840', 'Q16143', 'P10636-8']
-# protein_names = ['']
+# accession_numbers = ['P10636-8']
+accession_numbers = ['P10636-5', 'P10636-8']
+# protein_names = ['PRIO_HUMAN']
 # protein_names = ['SYUA_HUMAN']
-protein_names = ['TADBP_HUMAN']
+# protein_names = ['TADBP_HUMAN']
 # protein_names = ['SYUA_HUMAN', 'PRIO_HUMAN', 'URE2_YEAST', 'E9P8Q3_YEASX', 'TADBP_HUMAN']
-prot_id_seqs = read_seqs.get_sequences_by_uniprot_accession_nums_or_names(prot_ids=accession_numbers + protein_names)
+protein_names = ['']
+prot_ids = accession_numbers + protein_names
+prot_id_seqs = read_seqs.get_sequences_by_uniprot_accession_nums_or_names(prot_ids=prot_ids)
 
 # STEP 1 - Define property and corresponding parameters.
-# _property = Props.bSC.value
 _property = Props.bSC.value
-params = {'window_len_min': DefaultBSC.window_len_min.value,
-          'window_len_max': DefaultBSC.window_len_max.value,
-          'top_scoring_windows_num': DefaultBSC.top_scoring_windows_num.value,
-          'threshold': DefaultBSC.threshold.value}
+params = DefaultBSC.all_params.value
 
 # STEP 2 - salsa produces an array holding a single numbers for each residue.
 all_summed_scores = dict()
@@ -29,9 +29,8 @@ for prot_id, prot_seq in prot_id_seqs.items():
     summed_scores = execute.sum_scores_for_plot(scored_windows_all)
     all_summed_scores[prot_id] = summed_scores
 
-# STEP 3
-# currently only able to plot one protein per plot.
-execute.plot_summed_scores(all_summed_scores, _property, protein_names=list(all_summed_scores.keys()))
+# STEP 3 - Plot SALSA summed scores
+execute.plot_summed_scores(all_summed_scores, _property, prot_name_labels=list(all_summed_scores.keys()), params=params)
 
 # STEP 4 - Generate a single scalar representing the property of interest for the protein of interest.
 salsa_integrals = execute.integrate_salsa_plot(all_summed_scores)
