a084c238bd32f7d875052e88de7aa61cdbfd9348
zibaee-2021 <77154990+zibaee-2021@users.noreply.github.com>
2022-01-30 15:42:22 +0000

    Add function to convert amino acids (read from new enum) into dict.

--- data/aa_properties/read_props.py
+++ data/aa_properties/read_props.py
@@ -28,7 +28,12 @@ def read_aa_props_csv() -> pDF:
     return pd.read_csv(os.path.join(abs_path_aa_props, 'aa_props.csv'))
 
 
-def convert_alpha_beta_turn_pdf_to_dicts(aa_props: pDF) -> Tuple:
+def convert_electrostatics_pdf_to_dicts(aa_props: pDF) -> dict[str: int]:
+    charge = dict(zip(aa_props['aa'], aa_props['charge']))
+    return charge
+
+
+def convert_alpha_beta_turn_pdf_to_dicts(aa_props: pDF) -> Tuple[dict, dict, dict]:
     p_alpha = dict(zip(aa_props['aa'], aa_props['Palpha']))
     p_beta = dict(zip(aa_props['aa'], aa_props['Pbeta']))
     p_turn = dict(zip(aa_props['aa'], aa_props['Pturn']))
@@ -36,22 +41,23 @@ def convert_alpha_beta_turn_pdf_to_dicts(aa_props: pDF) -> Tuple:
 
 
 if __name__ == '__main__':
-    from matplotlib import pyplot as plt
-    import seaborn as sns
-    #
-    # In order to estimate a value for Proline in the Radzicka-Wolfenden scale, I am
-    #     making a visualisation of the other scales, after separating and sorting them
-    # Move this to a notebook in sandbox
-    hyd_sc = read_all_hydrophobicity_scales_csv()
-    hyd_sc_kd = hyd_sc[['aa', 'Kyte-Doolittle']].sort_values(by='Kyte-Doolittle')
-    # ax = sns.barplot(x='aa', y='Kyte-Doolittle', data=hyd_sc_kd)
-    sns.catplot(data=hyd_sc_kd, x='aa', y='Kyte-Doolittle', kind='point')
-    plt.show()
-    hyd_sc_hw = hyd_sc[['aa', 'Hopp-Woods']].sort_values(by='Hopp-Woods')
-    hyd_sc_co = hyd_sc[['aa', 'Cornette']].sort_values(by='Cornette')
-    hyd_sc_ei = hyd_sc[['aa', 'Eisenberg']].sort_values(by='Eisenberg')
-    hyd_sc_ro = hyd_sc[['aa', 'Rose']].sort_values(by='Rose')
-    hyd_sc_ja = hyd_sc[['aa', 'Janin']].sort_values(by='Janin')
-    hyd_sc_en = hyd_sc[['aa', 'Engelman_(GES)']].sort_values(by='Engelman_(GES)')
-    hyd_sc_rw = hyd_sc[['aa', 'Radzicka-Wolfenden']].sort_values(by='Radzicka-Wolfenden')
-    hyd_sc_rw
+
+    print(convert_electrostatics_pdf_to_dicts(read_aa_props_csv()))
+    # from matplotlib import pyplot as plt
+    # import seaborn as sns
+    # #
+    # # In order to estimate a value for Proline in the Radzicka-Wolfenden scale, I am
+    # #     making a visualisation of the other scales, after separating and sorting them
+    # # Move this to a notebook in sandbox
+    # hyd_sc = read_all_hydrophobicity_scales_csv()
+    # hyd_sc_kd = hyd_sc[['aa', 'Kyte-Doolittle']].sort_values(by='Kyte-Doolittle')
+    # # ax = sns.barplot(x='aa', y='Kyte-Doolittle', data=hyd_sc_kd)
+    # sns.catplot(data=hyd_sc_kd, x='aa', y='Kyte-Doolittle', kind='point')
+    # plt.show()
+    # hyd_sc_hw = hyd_sc[['aa', 'Hopp-Woods']].sort_values(by='Hopp-Woods')
+    # hyd_sc_co = hyd_sc[['aa', 'Cornette']].sort_values(by='Cornette')
+    # hyd_sc_ei = hyd_sc[['aa', 'Eisenberg']].sort_values(by='Eisenberg')
+    # hyd_sc_ro = hyd_sc[['aa', 'Rose']].sort_values(by='Rose')
+    # hyd_sc_ja = hyd_sc[['aa', 'Janin']].sort_values(by='Janin')
+    # hyd_sc_en = hyd_sc[['aa', 'Engelman_(GES)']].sort_values(by='Engelman_(GES)')
+    # hyd_sc_rw = hyd_sc[['aa', 'Radzicka-Wolfenden']].sort_values(by='Radzicka-Wolfenden')
