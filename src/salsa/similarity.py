0df2318cb1afde9d3c8c50af91954c2e869b5ec8
zibaee-2021 <77154990+zibaee-2021@users.noreply.github.com>
2022-02-02 01:37:20 +0000

    Add function to find peaks in SALSA plots.

--- src/salsa/similarity.py
+++ src/salsa/similarity.py
@@ -1,8 +1,13 @@
+from typing import Tuple
 import numpy as np
+
+from execute import compute
+import execute
+from data.protein_sequences import read_seqs
+from Options import Props, DefaultBSC
 """
-Generate a scalar score of similarity between two or more SALSA summed scores, that 
-gives an indication of similarity in terms of both the relative positions of peaks to each other
-as well as their magnitude (and therefore shape).
+Functions for detecting the similarity between two SALSA plots.
+The utility of this module is not determined or clear at this moment.
 """
 
 
@@ -53,4 +58,76 @@ def compute_sum_of_products_of_summed_scores(base_summed_scores: np.array, query
     return np.sum(sums_of_products, axis=0)
 
 
-    def find_peaks(summed_scores)
+def _find_peaks(prot_id_summed_scores: dict[str: np.array], min_cut_off: float) -> dict[str: Tuple[str]]:
+    """
+    For the given protein's summed scores, identify the start and end positions of peaks, identified as regions
+    scoring above the given cut off.
+    :param prot_id_summed_scores: SALSA summed scores mapped to the protein name/id.
+    :param min_cut_off: Ignore peaks that do not surpass this cut-off value.
+    :return: Peaks of the given SALSA summed scores, mapped to its protein id,
+    e.g. of the format: 'SYUA_HUMAN': (11-34, 56-59, 83-124)
+    """
+    tuple_of_peaks = ()
+    for prot_id, summed_scores in prot_id_summed_scores.items():
+        start_pos = None
+        is_peak = False
+        for i, score in enumerate(summed_scores):
+            if score > min_cut_off and not is_peak:
+                is_peak = True
+                if start_pos is None:
+                    start_pos = i + 1
+            elif score <= min_cut_off and is_peak:
+                end_pos = i + 1
+                is_peak = False
+                tuple_of_peaks += (str(start_pos) + '-' + str(end_pos), )
+                start_pos = None
+            elif is_peak and i == len(summed_scores) - 1:
+                end_pos = i + 1
+                tuple_of_peaks += (str(start_pos) + '-' + str(end_pos),)
+    prot_id, = prot_id_summed_scores.keys()
+    return {prot_id: tuple_of_peaks}
+
+
+def find_peaks(prot_ids_summed_scores: dict[str: np.array], min_cut_off: float) -> dict:
+    """
+    For the given proteins' summed scores, identify the start and end positions of peaks, identified as regions
+    scoring above the given cut off.
+    :param prot_ids_summed_scores: SALSA summed scores mapped to the proteins' names/ids.
+    :param min_cut_off: Ignore peaks that do not surpass this cut-off value.
+    :return: Peaks of the given SALSA summed scores mapped to the corresponding proteins,
+    e.g. of the format: 'SYUA_HUMAN': (11-34, 56-59, 83-124), 'SYUB_HUMAN': (11-34, 83-124)
+    """
+    prot_ids_peaks = dict()
+    for prot_id, summed_scores in prot_ids_summed_scores.items():
+        prot_ids_peaks.update(_find_peaks({prot_id: summed_scores}, min_cut_off))
+    return prot_ids_peaks
+
+
+def _detect_similarity_by_sharpened_cosine_distance():
+    """
+    A feature detector shown to have advantages over convolution by Brandon Rohrer in a tweet in 24Feb20, `@_brohrer_`.
+    It should allow you to run a peak from the SALSA plot of one protein over the SALSA plot of another protein by
+    sliding window and detect the similarity of peaks.
+    :return:
+    """
+    # TODO
+
+
+
+
+if __name__ == '__main__':
+    # prot_seq = read_seqs.read_protein_sequence_txt(read_seqs.TxtFiles.ASYN.value)
+    prot_ids = 'P10636-8'
+    prot_seq = read_seqs.get_sequences_by_uniprot_accession_nums_or_names(prot_ids=prot_ids)
+    scored_windows_all = compute(sequence=prot_seq[prot_ids], _property=Props.bSC.value,
+                                 params=DefaultBSC.all_params.value)
+    summed_scores = execute.sum_scores_for_plot(scored_windows_all)
+    blabla = dict()
+    blabla['protein_A'] = summed_scores
+
+    peaks = find_peaks(blabla, 10)
+    print(peaks)
+
+
+
+
