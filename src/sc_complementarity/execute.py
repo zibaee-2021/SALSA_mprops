07dbedbd8b6dee877ef36ec2997333a7770aaabf
zibaee-2021 <77154990+zibaee-2021@users.noreply.github.com>
2022-02-06 02:20:35 +0000

    Write logic for generating all possible conformations (although this currently becomes exponentially slow and CPU intensive above 9 residue-length sequences). Write logic for identifying neighbouring residues to every residue. Write unittests.

--- src/sc_complementarity/execute.py
+++ src/sc_complementarity/execute.py
@@ -51,6 +51,24 @@ some purpose for increasing the flexibility and
 model to go about this task. I will need to investigate this after first implementing the original
 idea.
 
+Each protein's possible `conformations` is a list of `conformation`. The list contains a tuple of tuples. Each inner
+tuple represents the coordinates of a residue, and the outer tuple simply contains all of these inner tuples. The
+reason for choosing to encapsulate `conformation` as a python tuple of tuples instead of a python list of tuples is
+because a tuple is immutable and as a result using less memory than a list, which is mutable. There is no requirement
+for changing any of the positions in each conformation so there is no need to use a list of tuples.
+E.g. if you have a 2-residue sequence like 'AC', then your `conformations` would look like:
+[ ((0, 0), (-1,  0)),
+  ((0, 0), (-1,  1)),
+  ((0, 0), ( 0,  1)),
+  ((0, 0), ( 1,  1)),
+  ((0, 0), ( 1,  0)),
+  ((0, 0), ( 1, -1)),
+  ((0, 0), ( 0, -1)),
+  ((0, 0), (-1, -1))]
+As in the illustration above, the first residue is anchored at one position (0, 0) and the neighbouring residue can now
+occupy each of 8 positions around the first residue. As such a 2-residue protein has 8 possible conformations. And so
+it is represented by a list of 8 tuples of tuples contain each pair of positional coordinates.
+
 ** NOTE **:
 You can generate all possible conformations without needing to know the sequence.
 In fact you can generate them in advance for any size of protein and then just fill it in
@@ -58,41 +76,83 @@ with the sequence later!
 """
 from __future__ import annotations
 # `annotations` because dataclass cannot recognise Node type self-reference.
-from dataclasses import dataclass
-import numpy as np
 from typing import List, Tuple
 from data.protein_sequences import read_seqs
+import time
+import math
+import pandas as pd
+
+# def _print(conformation):
+
+def _get_euclidean_dist(res_pos1, res_pos2):
+    return math.sqrt((res_pos1[0] - res_pos2[0]) ** 2 + (res_pos1[1] - res_pos2[1]) ** 2)
 
 
-@dataclass
-class Node:
-    aa: str = None
-    current_position: Tuple[int, int] = None
-    next_node: Node = None
-
-    def __eq__(self, other):
-        return self.aa == other.aa and \
-               self.next_node == other.next_node and \
-               self.current_position == other.current_position
+def _get_manhattan_dist(res_pos1, res_pos2):
+    return abs(res_pos1[0] - res_pos2[0]) + abs(res_pos1[1] - res_pos2[1])
 
 
-@dataclass
-class Conformation:
-    n_term: Node
-    complementarity_score: float = None
-
-    def __eq__(self, other):
-        return self.n_term == other.n_term and \
-               self.complementarity_score == other.complementarity_score
-
-
-def compute_complementarity_score(conformation: Conformation) -> float:
+def find_adjacent_residues(conformation: Tuple[Tuple]) -> Tuple[dict, dict]:
     """
-    Compute the mean of possible interactions from the given conformation.
-    :param conformation: Current conformation of the protein.
-    :return: Mean of possible interactions
+    From the given conformation, find the 8 residues that are in the immediate neighbourhood of each residue.
+    Find the 16 residues that are in the next neighbourhood out of each residue:
+
+                    o  o  o  o  o
+                    o  i  i  i  o
+                    o  i  .  i  o
+                    o  i  i  i  o
+                    o  o  o  o  o
+                                        . is central residue
+                                        i is immediate neighbours
+                                        o is outer neighbours
+
+    (Note: In current implementation, residues are only considered valid neighbours if they are not the next residue
+    along in sequence. This might not be a valid consideration and will possibly be removed subject to what is
+    reported and observed in amyloid structures in literature.)
+    :param conformation:
+    :return:
     """
-    return 4.4
+    adjacent, adjacent_outer = {}, {}
+    for x, residue in enumerate(conformation):
+        res_num = x + 1
+        for xx, residue_ in enumerate(conformation):
+            res_num_ = xx + 1
+            if abs(res_num - res_num_) > 1:
+                if _get_euclidean_dist(residue, residue_) <= math.sqrt(2):
+                    if adjacent.get(res_num, None) is not None:
+                        adjacent[res_num] += (res_num_, )
+                    else:
+                        adjacent[res_num] = (res_num_, )
+                elif _get_euclidean_dist(residue, residue_) <= math.sqrt(8):
+                    if adjacent_outer.get(res_num) is not None:
+                        adjacent_outer[res_num] += (res_num_, )
+                    else:
+                        adjacent_outer[res_num] = (res_num_, )
+                else:
+                    continue
+    return adjacent, adjacent_outer
+
+
+def _extract_unique_pairs(adjacent, adjacent_outer):
+    pairs = tuple()
+    for res_num, neighbours in adjacent.items():
+        for neighbour in neighbours:
+            pair = (res_num, neighbour)
+            pair = tuple(sorted(pair))
+            if pair not in pairs:
+                pairs += (pair, )
+    pairs_outer = tuple()
+    for res_num, neighbours in adjacent_outer.items():
+        for neighbour in neighbours:
+            pair = (res_num, neighbour)
+            pair = tuple(sorted(pair))
+            if pair not in pairs_outer:
+                pairs_outer += (pair, )
+    return pairs, pairs_outer
+
+
+
+
 
 
 def get_conformations(anchor_pos: Tuple, occupied: Tuple) -> list:
@@ -106,6 +166,8 @@ def get_conformations(anchor_pos: Tuple, occupied: Tuple) -> list:
     :return: All possible conformations for the given `anchor` residue and its C-terminal neighbour.
     """
     conformations = list()
+    if anchor_pos == (0, 0):
+        occupied = (occupied, )
     for rel_pos in [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]:
         neighbour_pos = (anchor_pos[0] + rel_pos[0], anchor_pos[1] + rel_pos[1])
         if neighbour_pos not in occupied:
@@ -113,16 +175,55 @@ def get_conformations(anchor_pos: Tuple, occupied: Tuple) -> list:
     return conformations
 
 
-# if __name__ == '__main__':
-#     print()
+def get_conformations(conformations: List[Tuple[Tuple[int, int]]]) -> list:
+    """
+    Generate all possible conformations for a residue relative to its N-terminal neighbour, termed the `anchor`.
+    This name is given because as far as the neighbour residue is concerned, the anchor remains fixed at its position
+    (for the purposes of this function).
+    Assumption here is that if `len(conformations) is one`, this can only the case for the first (N-terminal) residue.
+    Therefore `conformation` has only one position, because the N-terminal anchor residue never
+    moves. As such, I enclose it inside an empty tuple, otherwise `_get_conformations()` will fail due to its logic
+    expecting `conformation` to be a tuple of tuples.
+    :param conformations: Conformations
+    :return: All possible conformations for the given `anchor` residue and its C-terminal neighbour.
+    """
+    combined_confs = list()
+    for conformation in conformations:
+        anchor_pos = conformation[-1]
+        new_confs = _get_conformations(anchor_pos, occupied=conformation)
+        for i, new_conf in enumerate(new_confs):
+            new_conf = conformation[:-1] + new_conf
+            combined_confs.append(new_conf)
+    return combined_confs
 
 
+def build_conformations(seq: str, starting_conformations:List[Tuple[Tuple]]=None):
+    if starting_conformations is None:
+        all_protein_confs = []
+    else:
+        all_protein_confs = starting_conformations
+    for i, aa in enumerate(seq):
+        print(f'starting residue {aa} at pos {i + 1}')
+        print(f'{round(1000 * (time.time() - st), 1)} ms')
+        if i == 0:
+            conformations = [((0, 0),)]
+            all_protein_confs.extend(get_conformations(conformations))
+        else:
+            all_protein_confs.extend(get_conformations(all_protein_confs))
+    return all_protein_confs
+
+
+if __name__ == '__main__':
+    # print(multiprocessing.cpu_count())
+    st = time.time()
     # prot_id = 'P05067(672-711)'
     # prot_id_seq = read_seqs.get_sequences_by_uniprot_accession_nums_or_names(prot_id)
-    # res = init_conformation_(prot_id_seq[prot_id])
-    # seq_nodes_ = init_conformation_('ACD')
-    # unavailable_pos_ = [(0, 0)]
-    # seq_nodes_ = find_all_possible_positions_for_current_conformation(seq_nodes_, unavailable_pos_)
-    # scores_seq_nodes = dict()
-    # scores_seq_nodes[seq_nodes_] = compute_complementarity_score(seq_nodes_)
-    # update_to_next_conformation(seq_nodes_)
+    # seq = prot_id_seq[prot_id]
+    seq = 'DAEFRHDSGYEVHHQKLVFFAEDVGSNKGAIIGLMVGGVVIA'
+    res = build_conformations(seq[:8])
+
+    print(f'Number of possible conformations: {len(res)}')
+    # print(f'{round(1000 * (time.time() - st), 1)} ms')
+
+# First 8 residues is about 1 second.
+# 9th takes
--- test/sc_complementarity/test_execute.py
+++ test/sc_complementarity/test_execute.py
@@ -5,14 +5,6 @@ from dataclasses import dataclass
 
 class TestComp(TestCase):
 
-    def setUp(self) -> None:
-        node3 = execute.Node(aa='D')
-        node2 = execute.Node(aa='C')
-        self.node1 = execute.Node(aa='A', current_position=(0, 0))
-        self.node1.next_node = node2
-        node2.next_node = node3
-        self.conf = execute.Conformation(self.node1)
-
     def test_get_conformations(self):
         seq = 'AC'
         # note a residue is represented by a tuple
@@ -28,7 +20,7 @@ class TestComp(TestCase):
                     ((0, 0), (0, -1)),
                     ((0, 0), (-1, -1))]
 
-        actual = execute.get_conformations(anchor_pos, occupied=anchor_pos)
+        actual = execute._get_conformations(anchor_pos, occupied=anchor_pos)
         self.assertCountEqual(expected, actual)
 
     def test_get_conformations_3aa(self):
@@ -51,14 +43,58 @@ class TestComp(TestCase):
                     ((0, 0), (-1, 1), (0,  1)),
                     ((0, 0), (-1, 1), (-1, 0)),
                     ((0, 0), (-1, 1), (-2, 0))]
-        new_confs = list()
         combined_confs = list()
         for conformation in conformations:
             anchor_pos = conformation[-1]
-            new_confs = execute.get_conformations(anchor_pos, occupied=conformation)
+            new_confs = execute.get_conformations(anchor_pos)
             for i, new_conf in enumerate(new_confs):
                 new_conf = conformation[:-1] + new_conf
                 combined_confs.append(new_conf)
         actual = combined_confs
         self.maxDiff = None
         self.assertCountEqual(expected, actual)
+
+    def test_build_conformations(self):
+        actual = execute.build_conformations(seq='ACD')
+        expected = []
+        self.assertCountEqual(expected, actual)
+
+    def test__get_manhattan_dist(self):
+        res_pos1, res_pos2 = (3, 4), (6, 9)
+        expected = 8
+        actual = execute._get_manhattan_dist(res_pos1, res_pos2)
+        self.assertEqual(expected, actual)
+
+    def test__get_euclidean_dist(self):
+        res_pos1, res_pos2 = (3, 5), (6, 9)
+        expected = 5
+        actual = execute._get_euclidean_dist(res_pos1, res_pos2)
+        self.assertEqual(expected, actual)
+
+    def test_find_adjacent_residues(self):
+        conformation = ((0, 0), (-1, 1), (-1, 2), (-2, 3), (-3, 2), (-4, 1),
+                        (-3, 0), (-2, 0), (-1, -1), (-1, -2), (0, -1))
+        actual_adj, actual_adj_out = execute.find_adjacent_residues(conformation)
+        expected_adj = {1: (9, 11), 2: (8, ), 8: (2, ), 9: (1, 11), 11: (1, 9)}
+        expected_adj_outer = {1: (3, 8, 10), 2: (4, 5, 7, 9, 11), 3: (1, 5, 7, 8), 4: (2, 6), 5: (2, 3, 7, 8),
+                              6: (4, 8), 7: (2, 3, 5, 9, 10), 8: (1, 3, 5, 6, 10, 11), 9: (2, 7), 10: (1, 7, 8),
+                              11: (2, 8)}
+        self.assertDictEqual(expected_adj, actual_adj)
+        self.assertDictEqual(expected_adj_outer, actual_adj_out)
+
+    def test__extract_unique_pairs(self):
+        adj = {1: (9, 11), 2: (8, ), 8: (2, ), 9: (1, 11), 11: (1, 9)}
+        adj_outer = {1: (3, 8, 10), 2: (4, 5, 7, 9, 11), 3: (1, 5, 7, 8), 4: (2, 6), 5: (2, 3, 7, 8),
+                     6: (4, 8), 7: (2, 3, 5, 9, 10), 8: (1, 3, 5, 6, 10, 11), 9: (2, 7), 10: (1, 7, 8), 11: (2, 8)}
+        pairs_adj, pairs_adj_outer = execute._extract_unique_pairs(adj, adj_outer)
+        print(pairs_adj)
+        print(pairs_adj_outer)
+
+# def test_get_neighbouring_pairs(self):
+    #     conformation = ((0, 0), (-1, 0), (-2, 0), (-3, 0), (-2, 1), (-1, 2), (0, 1))
+    #     actual = execute.get_neighbouring_pairs(conformation)
+    #     expected = []
+
+    # def test__print(self):
+    #     conformation = ((0, 0), (-1, 0), (-2, 0), (-3, 0), (-2, 1), (-3, 2), (-2, 1), (-1, 1), (0, 1))
+    #     execute._print(conformation)
