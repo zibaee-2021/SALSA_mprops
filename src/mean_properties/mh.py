c4ca37ffd64ac7cf3616374866ad0a0b29fe2a7e
zibaee-2021 <kdb5og@83mfol.site>
2022-02-08 17:16:36 +0000

    Edit Proline R&W value in csv to -3.8 according to the original Java implementation. Accordingly, add comments in mh.py and update RWS enum proline value. Add extra note file in data/aa_properties. This value and indeed this scale will likely be replaced by another one, soon.

--- src/mean_properties/mh.py
+++ src/mean_properties/mh.py
@@ -3,6 +3,13 @@ import numpy as np
 
 
 def compute_mean_hydrophilicity(sequence: str):
+    """
+    Calculate the mean hydrophilicity of the given protein sequence, using Radzicka & Wolfenden hydrophilicity scale.
+    (The R&W scale lacked a value for Proline. It was estimated at -3.8 based on comparison with Roseman hydrophobicity
+    numbers.)
+    :param sequence: Protein sequence in 1-letter notation.
+    :return: Mean hydrophilicity of given protein.
+    """
     return np.mean([RWS.HYDROPHILICTY.value[aa] for aa in sequence])
 
 
@@ -11,7 +18,7 @@ class RWS(Enum):
 
     HYDROPHILICTY = {'A': -0.45, 'C': -3.63, 'D': -13.34, 'E': -12.63, 'F': -3.15,
                      'G': 0, 'H': -12.66, 'I': -0.24, 'K': -11.91, 'L': -0.11,
-                     'M': -3.87, 'N': -12.07, 'P': 0.1, 'Q': -11.77, 'R': -22.31,
+                     'M': -3.87, 'N': -12.07, 'P': -3.8, 'Q': -11.77, 'R': -22.31,
                      'S': -7.45, 'T': -7.27, 'V': -0.40, 'W': -8.27, 'Y': -8.50}
 
 
