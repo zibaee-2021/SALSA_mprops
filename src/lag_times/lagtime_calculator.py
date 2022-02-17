eeb0a54c0efc68e648bdf963fde14b3bde3a3db3
zibaee-2021 <kdb5og@83mfol.site>
2022-02-17 02:57:55 +0000

    Add functions to remove unusable data and to take mean of remaining data. Add unit tests.

--- src/lag_times/lagtime_calculator.py
+++ src/lag_times/lagtime_calculator.py
@@ -1,13 +1,42 @@
-import math
 import os
 import csv
 import numpy as np
+import pandas as pd
 from root_path import abspath_root
-from typing import Tuple
+from typing import Tuple, List
 import matplotlib.pyplot as plt
 import re
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+
+
+def _calc_mean_of_lagtimes(lagtimes: dict[str: List]) -> dict[str: float]:
+    """
+    Calculate the mean of the 'lag times' of each synuclein. It is expected that this function is called only on the
+    cleaned 'lag times', such that only numerical values are included (`NA` is already removed) and synucleins with
+    less than 3 lag times are also removed.
+    :param lagtimes: 'Lag times' of synucleins with 3 or more values and without any `NA`, mapped to the synuclein name.
+    :return: Mean of the given 'lag times' mapped to the synuclein name.
+    """
+    return {syn: round(np.mean(lags), 1) for syn, lags in lagtimes.items()}
+
+
+def _process_na(lagtimes: dict[str: List]) -> dict[str: List]:
+    """
+    Remove NAs from the lag times of each synuclein. If the synuclein is left with less than 3 lag times, it is
+    removed from dataset.
+    :param lagtimes: Lag times for each synuclein including `NA` (indicating no filament assembly within 100 h).
+    :return: Lag times of synucleins that have 3 or more lag times.
+    """
+    lagtimes_ = {}
+    for syn, lags in lagtimes.items():
+        lags_ = [lag for lag in lags if lag != 'NA']
+        lagtimes_[syn] = lags_
+    return {syn: lags for syn, lags in lagtimes_.items() if len(lags) >= 3}
+
+
+def clean_and_mean(lagtimes):
+    return _calc_mean_of_lagtimes(_process_na(lagtimes))
 
 
 def _solve_for_y(poly_coeffs, intercept, y):
@@ -31,19 +60,19 @@ def _plot(preproc, lin_reg, X, y, y_pred, name):
     plt.show()
 
 
-def _calculate_lag_times(name, time, tht, lagtimes, plot):
+def _calculate_lag_times(name: str, time: list, tht: list, lagtimes: dict, plot: bool, degree: int):
     if tht is None:
         lag_hours = 'NA'
     else:
-        X = np.array([num for i, num in enumerate(time) if i == 0 or num != 0.0]).reshape((-1, 1))
-        y = np.array(tht[0: len(X)])
-        poly_reg = PolynomialFeatures(degree=6)
-        X_poly = poly_reg.fit_transform(X)
+        x = np.array([num for i, num in enumerate(time) if i == 0 or num != 0.0]).reshape((-1, 1))
+        y = np.array(tht[0: len(x)])
+        poly_reg = PolynomialFeatures(degree=2)
+        X_poly = poly_reg.fit_transform(x)
         lin_reg = LinearRegression()
         lin_reg.fit(X_poly, y)
         if plot:
             y_pred = lin_reg.predict(X_poly)
-            _plot(preproc=poly_reg, lin_reg=lin_reg, X=X, y=y, y_pred=y_pred, name=name)
+            _plot(preproc=poly_reg, lin_reg=lin_reg, X=x, y=y, y_pred=y_pred, name=name)
         lag_hours = np.real(_solve_for_y(lin_reg.coef_, 4.0, 16.0))
         lag_hours = abs(round(float(lag_hours), 1))
     if name not in lagtimes:
@@ -53,10 +82,10 @@ def _calculate_lag_times(name, time, tht, lagtimes, plot):
     return lagtimes
 
 
-def _include_lag_phase_only(X: np.array, y: np.array) -> Tuple[np.array, np.array]:
+def _include_lag_phase_only(x: np.array, y: np.array) -> Tuple[np.array, np.array]:
     for i, tht in enumerate(y):
         if tht >= 16.0:
-            return X[i - 1: i + 1], y[i - 1: i + 1]
+            return x[i - 1: i + 1], y[i - 1: i + 1]
     return None, None
 
 
@@ -77,7 +106,7 @@ def _get_data():
             yield row
 
 
-def get_lagtimes():
+def get_lagtimes(plot: bool, degree: int) -> dict[str: List]:
     """
     Read `translatedTo4.csv` file and determine the time taken for each protein's ThT fluorescence to reach the
     square of its starting value, which has been translated to 4.0. This is referred to here as a "lag time".
@@ -86,9 +115,11 @@ def get_lagtimes():
     row down by `Time (h)` in the first column and the names of the synuclein constructs in the adjoining columns.
     The subsequent time points at which measurements were taken and the corresponding ThT fluorescence for each
     construct follow in the rows below.
-    :return: "Lag times"
+    :param plot: True to generate a plot of the region of the ThT fluorescence curve that includes the value by which
+    the 'lag time' is deemed to have ended.
+    :param degree: The degree to use for the polynomial regression of the ThT fluorescence data.
+    :return: Each synuclein name mapped to its 'lag times'.
     """
-    tht_date = ''
     syn_names = ()
     time = np.zeros(10)
     tht = None
@@ -99,8 +130,7 @@ def get_lagtimes():
     i = 0
     for row in data_:
         if _is_date(row[0]):
-            tht_date = row[0]
-            print(tht_date)
+            continue
         elif row[0] == 'Time (h)':
             syn_names += tuple(filter(None, row[1:]))
             tht = np.zeros((10, len(syn_names)))
@@ -110,15 +140,36 @@ def get_lagtimes():
             time[i] = row[0]
         elif row[0] == '':
             for i, name in enumerate(syn_names):
-                time_, tht_ = _include_lag_phase_only(X=time, y=tht[:, i])
-                lagtimes = _calculate_lag_times(name=name, time=time_, tht=tht_, lagtimes=lagtimes, plot=False)
+                time_, tht_ = _include_lag_phase_only(x=time, y=tht[:, i])
+                lagtimes = _calculate_lag_times(name=name, time=time_, tht=tht_,
+                                                lagtimes=lagtimes, plot=plot, degree=degree)
             syn_names = ()
         else:
-            tht[i, ] = [float(n) if n != '' else 0.0 for n in row[1:len(syn_names) + 1]]
+            tht[i, ] = [float(n) if n != '' else 0.0 for n in row[1: len(syn_names) + 1]]
             time[i] = row[0]
         i += 1
     return lagtimes
 
 
+def write_lag_time_means(lag_time_means: dict[str: float]):
+    col_names = ['Synucleins', 'lagtime_means']
+    df = pd.DataFrame.from_dict(data=lag_time_means, orient='index', columns=[col_names[1]])
+    df[col_names[0]] = df.index
+    df = df[col_names]
+    df.reset_index(drop=True, inplace=True)
+
+    lagtime_csv = os.path.join(abspath_root, 'data', 'tht_data', 'lagtime_means.csv')
+    df.to_csv(lagtime_csv, index=False)
+
+
 if __name__ == '__main__':
-    print(get_lagtimes())
+    _2 = clean_and_mean(get_lagtimes(plot=False, degree=2))
+    print(f'degree 2: {_2}')
+    _3 = clean_and_mean(get_lagtimes(plot=False, degree=3))
+    print(f'degree 3: {_3}')
+    _4 = clean_and_mean(get_lagtimes(plot=False, degree=4))
+    print(f'degree 4: {_4}')
+    _5 = clean_and_mean(get_lagtimes(plot=False, degree=5))
+    print(f'degree 5: {_5}')
+    _6 = clean_and_mean(get_lagtimes(plot=False, degree=6))
+    print(f'degree 6: {_6}')
--- test/lag_times/test_lagtime_calculator.py
+++ test/lag_times/test_lagtime_calculator.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from src.lag_times import lagtime_calculator
+from src.lag_times import lagtime_calculator as lagcalc
 
 
 class TestLagTimeCalculator(TestCase):
@@ -7,8 +7,39 @@ class TestLagTimeCalculator(TestCase):
     def test__include_lag_phase_only(self):
         X = [0, 4, 8, 24, 48, 96]
         y = [4.0, 6.1, 12.4, 16.0, 97.5, 123.6]
-        actual = lagtime_calculator._include_lag_phase_only(X, y)
+        actual = lagcalc._include_lag_phase_only(X, y)
         expected = [8, 24], [12.4, 16.0]
         self.assertTupleEqual(expected, actual)
         self.assertCountEqual(expected[0], actual[0])
         self.assertCountEqual(expected[1], actual[1])
+
+    def test_process_na(self):
+        lagtimes = {'asyn': [80, 60]}
+        actual = lagcalc._process_na(lagtimes)
+        expected = {}
+        self.assertDictEqual(expected, actual)
+
+        lagtimes = {'asyn': [80, 60, 'NA']}
+        actual = lagcalc._process_na(lagtimes)
+        expected = {}
+        self.assertDictEqual(expected, actual)
+
+        lagtimes = {'asyn': [80, 60, 70, 'NA']}
+        actual = lagcalc._process_na(lagtimes)
+        expected = {'asyn': [80, 60, 70]}
+        self.assertDictEqual(expected, actual)
+
+        lagtimes = {'asyn': [80, 'NA', 60, 'NA', 80]}
+        actual = lagcalc._process_na(lagtimes)
+        expected = {'asyn': [80, 60, 80]}
+        self.assertCountEqual(expected['asyn'], actual['asyn'])
+
+    def test_calc_mean_of_lagtimes(self):
+        lagtimes = {'syn': [20, 30, 40, 10]}
+        actual = lagcalc._calc_mean_of_lagtimes(lagtimes)
+        expected = {'syn': 25}
+        self.assertEqual(expected, actual)
+
+    def test_write_lag_time_means(self):
+        lag_time_means = {'syn': 23}
+        actual = lagcalc.write_lag_time_means(lag_time_means)
