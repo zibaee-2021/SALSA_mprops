bd1ec4b0d85e4d398df761cc9add7fa573cba61b
zibaee-2021 <kdb5og@83mfol.site>
2022-02-19 02:09:26 +0000

    Replace hardcoded lag_time namedtuples with reading directly from csv into pandas df. Use lowercase for var name. Change docstring to replace `32` synucleins with `41`.

--- src/mean_properties/mprops.py
+++ src/mean_properties/mprops.py
@@ -1,7 +1,8 @@
+import os
 from typing import Tuple, NamedTuple
+import pandas as pd
 from src.mean_properties import mbp, mh, mnc_mtc
 from collections import namedtuple
-from enum import Enum
 import math
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -9,11 +10,10 @@ from src.salsa import execute
 from src.salsa.Options import DefaultBSC, Props
 from data.protein_sequences import read_seqs
 from src.mutation import mutator
+from root_path import abspath_root
 """
 The derivation of the coefficients for each of the 4 mean properties (mean beta-sheet propensity, mean hydrophilicity, 
-mean absolute net charge and mean total charge) is based on the kinetics of fibrillogenesis for 32 (although .. it 
-may be more.. I don't remember why I didn't include the 45v46v71e72e mutants)
-recombinant 
+mean absolute net charge and mean total charge) is based on the kinetics of fibrillogenesis for 39 recombinant 
 synuclein constructs measured in vitro. The calculations are as described in Zibaee et al 2010 JBC (and in the 
 docstrings of the relevant functions below). 
 The combination of the 4 weighted properties into one is referred to as `mprops`. 
@@ -55,13 +55,14 @@ I may also do this for the large number of other constructs for which assembly w
 # bytes, while the dict equivalent uses about 1200 bytes. Hence namedtuple uses about 1/4 the memory of dict.
 # while the dict equivalent. An enum cannot be used as it does not allow duplicate values (i.e. it will not store
 # two syns that have the same lag times).
-# lagtimes = namedtuple('lagtimes', 'asyn asyn_11_140, asyn_21_140, asyn_31_140, asyn_41_140, asyn_51_140, asyn_61_140, '
+# lag_times = namedtuple('lag_times', 'asyn asyn_11_140, asyn_21_140, asyn_31_140, asyn_41_140, asyn_51_140,
+# asyn_61_140, '
 #                                   'asyn_1_80, asyn_del68_71, asyn_del71_72, asyn_del71_74, asyn_del71_78, '
 #                                   'asyn_del71_81, asyn_del73_83, bsyn_asyn_1, bsyn_asyn_12, bsyn_5V, bsyn_5V2Q, '
 #                                   'bsyn_5V4Q, bsyn_5V6Q, bsyn_5V8Q, asyn_T72V, asyn_V71E_T72E, asyn_A30P, asyn_E46K, '
 #                                   'asyn_A53T, asyn_S87E, asyn_S129E, bsyn_1_73, fr_asyn, fr_gsyn1, fr_gsyn2')
 
-# orig_lagtimes = lagtimes(asyn=5.5, asyn_11_140=27.5, asyn_21_140=10.75, asyn_31_140=27.5, asyn_41_140=54.0,
+# orig_lag_times = lag_times(asyn=5.5, asyn_11_140=27.5, asyn_21_140=10.75, asyn_31_140=27.5, asyn_41_140=54.0,
 #                     asyn_51_140=86.5, asyn_61_140=96.5, asyn_1_80=4.75, asyn_del68_71=41.5, asyn_del71_72=8.25,
 #                     asyn_del71_74=9.5, asyn_del71_78=52.75, asyn_del71_81=44.25, asyn_del73_83=19.75,
 #                     bsyn_asyn_1=81.25, bsyn_asyn_12=25.0, bsyn_5V=81.5, bsyn_5V2Q=20.25, bsyn_5V4Q=20.0,
@@ -70,47 +71,6 @@ I may also do this for the large number of other constructs for which assembly w
 #                     fr_gsyn1=0.75, fr_gsyn2=0.25)
 
 # Note: You cannot use hyphens in namedtuples, so I replaced all hyphens with underscores.
-lagtimes = namedtuple('lagtimes', 'asyn, aS87E, aS129E, a1_80, b1_73, gsyn, ga, a11_140, a21_140, ba1, a73_83Del, '
-                                  'a31_140, a41_140, a51_140, a68_71Del, a74_84Del, a61_140, fr_asyn, b5V, fr_gsyn2, '
-                                  'fr_gsyn1, b5V4Q, b5V6Q, b5V8Q, aE46K, aA30P, aA53T, ba12, a1_75, a71_72Del, aT72V, '
-                                  'a71_74Del, a71E72E, a71_81Del, a45V46V71E72E, fr_bsyn, a45V46V, bE46V, b45V46V, '
-                                  'aK45V, aE46V')
-
-lagtimes_deg2 = lagtimes(asyn=11.3, aS87E=19.8, aS129E=8.1, a1_80=51.8, b1_73=14.6, gsyn=14.6, ga=14.4, a11_140=23.5,
-                         a21_140=12.0, ba1=98.7, a73_83Del=20.4, a31_140=29.1, a41_140=37.7,  a51_140=58.4,
-                         a68_71Del=43.7, a74_84Del=23.1, a61_140=37.8, fr_asyn=1.4, b5V=88.9, fr_gsyn2=1.4,
-                         fr_gsyn1=1.6, b5V4Q=30.5, b5V6Q=17.2, b5V8Q=20.6, aE46K=4.2, aA30P=9.0, aA53T=7.8,
-                         ba12=18.8, a1_75=7.4, a71_72Del=4.1, aT72V=2.4, a71_74Del=7.4, a71E72E=77.2, a71_81Del=43.0,
-                         a45V46V71E72E=22.4, fr_bsyn=63.6, a45V46V=5.4, bE46V=39.3, b45V46V=37.5, aK45V=4.8, aE46V=3.0)
-
-lagtimes_deg3 = lagtimes(asyn=7.2, aS87E=12.2, aS129E=5.3, a1_80=23.9, b1_73=9.2, gsyn=10.8, ga=10.2, a11_140=16.7,
-                         a21_140=8.5, ba1=56.0, a73_83Del=12.1, a31_140=17.3, a41_140=24.0,  a51_140=33.9,
-                         a68_71Del=28.1, a74_84Del=13.9, a61_140=18.3, fr_asyn=1.0, b5V=46.6, fr_gsyn2=1.0,
-                         fr_gsyn1=1.0, b5V4Q=16.6, b5V6Q=11.2, b5V8Q=13.3, aE46K=3.3, aA30P=5.7, aA53T=4.7,
-                         ba12=15.0, a1_75=4.0, a71_72Del=3.7, aT72V=1.5, a71_74Del=5.6, a71E72E=41.8, a71_81Del=27.0,
-                         a45V46V71E72E=14.4, fr_bsyn=30.3, a45V46V=2.8, bE46V=21.2, b45V46V=21.1, aK45V=3.4, aE46V=1.8)
-
-lagtimes_deg4 = lagtimes(asyn=16.1, aS87E=26.4, aS129E=12.0, a1_80=45.1, b1_73=20.3, gsyn=26.0, ga=24.0, a11_140=39.4,
-                         a21_140=19.9, ba1=117.9, a73_83Del=26.0, a31_140=37.1, a41_140=53.9, a51_140=72.3,
-                         a68_71Del=62.6, a74_84Del=30.0, a61_140=35.4, fr_asyn=2.2, b5V=94.5, fr_gsyn2=2.1,
-                         fr_gsyn1=2.2, b5V4Q=34.0, b5V6Q=25.3, b5V8Q=29.8, aE46K=8.1, aA30P=12.7, aA53T=10.4,
-                         ba12=37.1, a1_75=8.2, a71_72Del=9.8, aT72V=3.2, a71_74Del=13.8, a71E72E=85.0, a71_81Del=59.4,
-                         a45V46V71E72E=32.2, fr_bsyn=58.2, a45V46V=5.7, bE46V=43.3, b45V46V=44.1, aK45V=7.8, aE46V=4.0)
-
-lagtimes_deg5 = lagtimes(asyn=13.9, aS87E=22.3, aS129E=10.4, a1_80=35.1, b1_73=17.4, gsyn=23.4, ga=21.4, a11_140=35.0,
-                         a21_140=17.7, ba1=98.0, a73_83Del=21.9, a31_140=31.1, a41_140=46.8, a51_140=60.6,
-                         a68_71Del=53.6, a74_84Del=25.4, a61_140=28.0, fr_asyn=1.9, b5V=77.0, fr_gsyn2=1.9,
-                         fr_gsyn1=1.9, b5V4Q=27.8, b5V6Q=22.0, b5V8Q=25.7, aE46K=7.4, aA30P=10.9, aA53T=8.9,
-                         ba12=34.0, a1_75=6.7, a71_72Del=9.4, aT72V=2.7, a71_74Del=12.5, a71E72E=69.0, a71_81Del=50.6,
-                         a45V46V71E72E=27.7, fr_bsyn=45.8, a45V46V=4.5, bE46V=35.3, b45V46V=36.4, aK45V=6.9, aE46V=3.3)
-
-lagtimes_deg6 = lagtimes(asyn=17.9, aS87E=28.4, aS129E=13.4, a1_80=42.2, b1_73=22.2, gsyn=31.1, ga=28.3,
-                         a11_140=45.9, a21_140=23.2, ba1=123.2, a73_83Del=27.8, a31_140=39.3, a41_140=60.5,
-                         a51_140=76.8, a68_71Del=68.7, a74_84Del=32.3, a61_140=34.0, fr_asyn=2.5, b5V=95.5,
-                         fr_gsyn2=2.4, fr_gsyn1=2.5, b5V4Q=34.6, b5V6Q=28.5, b5V8Q=33.2, aE46K=10.0, aA30P=14.1,
-                         aA53T=11.4, ba12=45.7, a1_75=8.3, a71_72Del=13.0, aT72V=3.4, a71_74Del=16.7, a71E72E=85.3,
-                         a71_81Del=64.6, a45V46V71E72E=35.6, fr_bsyn=55.4, a45V46V=5.7, bE46V=43.7, b45V46V=45.6,
-                         aK45V=9.1, aE46V=4.2)
 
 coefs_4props = namedtuple('coefficients_4props', 'nmbp_cf, nmh_cf, nmnc_cf, nmtc_cf')
 # These are the original coefficients from Zibaee et al 2010 JBC.
@@ -161,44 +121,42 @@ def _build_syn_sequences(syn_names: list) -> dict:
     fr_bsyn = read_seqs.get_sequences_by_uniprot_accession_nums_or_names('SYUB_FUGU')['SYUB_FUGU']
     fr_gsyn1 = read_seqs.get_sequences_by_uniprot_accession_nums_or_names('SYUG1_FUGU')['SYUG1_FUGU']
     fr_gsyn2 = read_seqs.get_sequences_by_uniprot_accession_nums_or_names('SYUG2_FUGU')['SYUG2_FUGU']
-    b5V = mutator.mutate(prot_seq=bsyn, pos_aa={11: 'V', 19: 'V', 63: 'V', 78: 'V', 102: 'V'})
-    syn_seqs = {'asyn': asyn, 'bsyn': bsyn, 'gsyn': gsyn, 'fr_asyn': fr_asyn, 'fr_gsyn1': fr_gsyn1,
-                'fr_gsyn2': fr_gsyn2, 'b5V': b5V}
+    b5v = mutator.mutate(prot_seq=bsyn, pos_aa={11: 'V', 19: 'V', 63: 'V', 78: 'V', 102: 'V'})
+    syn_seqs = {'asyn': asyn, 'gsyn': gsyn, 'fr_asyn': fr_asyn, 'fr_gsyn1': fr_gsyn1,
+                'fr_gsyn2': fr_gsyn2, 'b5V': b5v}
     for syn_name in syn_names:
         syn_seqs_keys = list(syn_seqs)
         if syn_name not in syn_seqs_keys:
             if syn_name in ['a11_140', 'a21_140', 'a31_140', 'a41_140', 'a51_140', 'a61_140', 'a71_140', 'a1_75',
                             'a1_80', 'b1_73', 'a68_71Del', 'a71_72Del', 'a71_72Del', 'a71_74Del', 'a71_78Del',
-                            'a71_81Del', 'a73_83Del', 'a74_84Del']
+                            'a71_81Del', 'a73_83Del', 'a74_84Del']:
                 syn_seqs[syn_name] = _make_fragment(syn_name)
-            elif syn_name == 'ba1': # bsyn1-72, asyn73-83, bsyn73-134 ..??
+            elif syn_name == 'bsyn':
+                syn_seqs[syn_name] = bsyn
+            elif syn_name == 'ba1':  # bsyn1-72, asyn73-83, bsyn73-134 ..??
                 syn_seqs[syn_name] = bsyn[:72] + asyn[72:83] + bsyn[72:]
-            elif syn_name == 'ba12': # asyn1-96, bsyn49 c-terminal residues...??
+            elif syn_name == 'ba12':  # asyn1-96, bsyn49 c-terminal residues...??
                 syn_seqs[syn_name] = asyn[:96] + bsyn[85:]
-
             elif syn_name == 'ga':  # THIS ONE NEEDS TO BE CHECKED - IN MPROPS FILE
                 syn_seqs[syn_name] = gsyn[:72] + asyn[72:83] + gsyn[72:]
-
             elif syn_name == 'b5V':
-                syn_seqs[syn_name] = b5V
+                syn_seqs[syn_name] = b5v
             elif syn_name == 'b5V2Q':
-                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5V, pos_aa={125: 'Q', 126: 'Q'})
+                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5v, pos_aa={125: 'Q', 126: 'Q'})
             elif syn_name == 'b5V4Q':
-                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5V, pos_aa={104: 'Q', 105: 'Q', 125: 'Q', 126: 'Q'})
+                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5v, pos_aa={104: 'Q', 105: 'Q', 125: 'Q', 126: 'Q'})
             elif syn_name == 'b5V6Q':
-                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5V, pos_aa={96: 'Q', 97: 'Q', 104: 'Q', 105: 'Q',
+                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5v, pos_aa={96: 'Q', 97: 'Q', 104: 'Q', 105: 'Q',
                                                                           125: 'Q', 126: 'Q'})
             elif syn_name == 'b5V8Q':
-                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5V, pos_aa={87: 'Q', 88: 'Q', 96: 'Q', 97: 'Q',
+                syn_seqs[syn_name] = mutator.mutate(prot_seq=b5v, pos_aa={87: 'Q', 88: 'Q', 96: 'Q', 97: 'Q',
                                                                           104: 'Q', 105: 'Q', 125: 'Q', 126: 'Q'})
-
             elif syn_name == 'aA30P':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={30: 'P'})
             elif syn_name == 'aE46K':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={46: 'K'})
             elif syn_name == 'aA53T':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={53: 'T'})
-
             elif syn_name == 'aK45V':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={45: 'V'})
             elif syn_name == 'aE46V':
@@ -209,21 +167,18 @@ def _build_syn_sequences(syn_names: list) -> dict:
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={45: 'V', 46: 'V', 71: 'E', 72: 'E'})
             elif syn_name == 'a71E72E':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={71: 'E', 72: 'E'})
-
             elif syn_name == 'bR45V':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=bsyn, pos_aa={45: 'V'})
             elif syn_name == 'b45V46V':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=bsyn, pos_aa={45: 'V', 46: 'V'})
             elif syn_name == 'bE46V':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=bsyn, pos_aa={46: 'V'})
-
             elif syn_name == 'aT72V':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={72: 'V'})
             elif syn_name == 'aS87E':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={87: 'E'})
             elif syn_name == 'aS129E':
                 syn_seqs[syn_name] = mutator.mutate(prot_seq=asyn, pos_aa={129: 'E'})
-
             elif syn_name == 'fr_asyn':
                 syn_seqs[syn_name] = fr_asyn
             elif syn_name == 'fr_bsyn':
@@ -242,7 +197,7 @@ def _normalise_mbp_mh_mnc_mtc(mbp: float, mh: float, mnc: float, mtc: float) -> 
     """
     Based on calculations from Zibaee et al 2007 JBC, each of the 4 values were normalised to spread to an approx
     range of 0-100. The calculations were based on the lowest and highest of each of these scores of the sequences of
-    the 32 Synuclein constructs included. They were included if they began to assemble into filaments in vitro
+    the 41 Synuclein constructs included. They were included if they began to assemble into filaments in vitro
     within 96 hours if the
     experiments had been repeated 3 or more times (from different recombinant protein preparations).
     :param mbp: Non-normalised mean beta-sheet propensity.
@@ -264,35 +219,35 @@ def calculate_relative_weights_for_each_4_props() -> Tuple[NamedTuple, NamedTupl
     The function should only need to be run once to generate the necessary models for predictions.
 
     The coefficients of each of following 4 plots were used for combining the 4 properties into one equation.
-    The 4 plots were of nmbp, nmh, nmnc, nmtc against natural log of the lag-times data of 32 **TODO more than 32**
-    recombinant synuclein constructs.
+    The 4 plots were of nmbp, nmh, nmnc, nmtc against natural log of the lag-times data of 41 recombinant synuclein
+    constructs.
 
-    The lag-times were the time taken for the ThT value to become the squared of its value at time = 0. The 32
+    The lag-times were the time taken for the ThT value to become the squared of its value at time = 0. The 41
     synuclein constructs that were included satisfied the following two requirements:
     1. They assembled with a detectable lag-time within 96 hours;
     2. They had been assayed ≥ 3 times (each from a different protein preparation batch).
     :return:
     """
-    syns_ln_lagtimes = {}
-    for syn, lagtime in zip(lagtimes._fields, lagtimes):
-        syns_ln_lagtimes[syn] = math.log(lagtime)
+    lag_times = pd.read_csv(os.path.join(abspath_root, 'data', 'tht_data', 'lag_time_degree_4.csv'))
+    lag_times['ln_lags'] = lag_times.apply(lambda row: math.log(row['lag_time_means']), axis=1)
+    syns = list(lag_times.Synucleins)
+    syn_seqs = _build_syn_sequences(syns)
+    x_nmbp, x_nmh, x_nmnc, x_nmtc, y = np.zeros(len(syn_seqs)), np.zeros(len(syn_seqs)), np.zeros(len(syn_seqs)), \
+                                       np.zeros(len(syn_seqs)), np.zeros(len(syn_seqs))
 
-    syn_seqs = _build_syn_sequences(list(lagtimes._fields))
-    x_nmbp, x_nmh, x_nmnc, x_nmtc, y = np.zeros(len(syn_seqs)), np.zeros(len(syn_seqs)), np.zeros(len(syn_seqs)),\
-                                   np.zeros(len(syn_seqs)), np.zeros(len(syn_seqs))
+    for i, (syn_name, seq) in enumerate(syn_seqs.items()):
+        x_nmbp[i], x_nmh[i], x_nmnc[i], x_nmtc[i] = _normalise_mbp_mh_mnc_mtc(
+            mbp=mbp.compute_mean_beta_sheet_prop(seq), mh=mh.compute_mean_hydrophilicity(seq),
+            mnc=mnc_mtc.compute_mean_net_charge(seq), mtc=mnc_mtc.compute_mean_total_charge(seq))
+        y[i] = float(lag_times.loc[syn_name == lag_times.Synucleins]['ln_lags'])
 
-    for i, (syn_name, seq), in enumerate(syn_seqs.items()):
-        x_nmbp[i], x_nmh[i], x_nmnc[i], x_nmtc[i] = _normalise_mbp_mh_mnc_mtc(mbp=mbp.compute_mean_beta_sheet_prop(seq),
-                                                                              mh=mh.compute_mean_hydrophilicity(seq),
-                                                                              mnc=mnc_mtc.compute_mean_net_charge(seq),
-                                                                              mtc=mnc_mtc.compute_mean_total_charge(seq))
-        y[i] = syns_ln_lagtimes[syn_name]
-
-    x_nmbp, x_nmh, x_nmnc, x_nmtc = x_nmbp.reshape((-1, 1)), x_nmh.reshape((-1, 1)),\
+    x_nmbp, x_nmh, x_nmnc, x_nmtc = x_nmbp.reshape((-1, 1)), x_nmh.reshape((-1, 1)), \
                                     x_nmnc.reshape((-1, 1)), x_nmtc.reshape((-1, 1))
 
     model = LinearRegression()
     model.fit(x_nmbp, y)
+    # BEWARE PYCHARM IS CURRENTLY GIVING NONETYPES ERROR WHEN IN DEBUGGER MODE. APPARENTLY THIS
+    # IS A PYCHARM BUG SPECIFIC TO LATEST PYTHON 3.10. (SIMILAR PYCHARM BUG WITH MATPLOTLIB.)
     mbp_rsq = model.score(x_nmbp, y)
     mbp_cf = float(model.coef_)
     mbp_ic = float(model.intercept_)
@@ -372,8 +327,8 @@ def predict_lag_time_with_combined_algo(syn_seqs):
 
 
 if __name__ == '__main__':
-    calculate_relative_weights_for_each_4_props()
-
+    _4coefs_, _4intcpts_, _4rsq_ = calculate_relative_weights_for_each_4_props()
+    print(f' _4coefs_ {_4coefs_}, _4intcpts_ {_4intcpts_}, _4rsq_ {_4rsq_}')
     # print(f' dict {basics.get_size_of(lagtimes)}')
     # print(f'namedtuple {basics.get_size_of(lagtimes)}')
 
