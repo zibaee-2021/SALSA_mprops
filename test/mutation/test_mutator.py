da6805d3320adbba1b49f8c505b3e8f0be9cd781
zibaee-2021 <kdb5og@83mfol.site>
2022-03-08 11:45:59 +0000

    Update reference to module `mutator`.

--- test/mutation/test_mutator.py
+++ test/mutation/test_mutator.py
@@ -4,7 +4,7 @@ from src.mutation import mutator
 from data.protein_sequences import read_seqs
 
 
-class TestMutate(TestCase):
+class TestMutator(TestCase):
 
     def setUp(self) -> None:
         self.asyn = 'MDVFMKGLSKAKEGVVAAAEKTKQGVAEAAGKTKEGVLYVGSKTKEGVVHGVATVAEKTKEQVTNVGGAV' \
@@ -13,7 +13,7 @@ class TestMutate(TestCase):
         self.prot_id_seq = {self.prot_id: self.asyn}
 
     def test__make_mutant_name_simple(self):
-        actual = mutate._make_mutant_name(prot_id_seq={'simple': 'AAP'}, point_mutation_to_make={3: 'D'})
+        actual = mutator._make_mutant_name(prot_id_seq={'simple': 'AAP'}, point_mutation_to_make={3: 'D'})
         expected = 'simple(P3D)'
         self.assertEqual(expected, actual)
 
@@ -21,22 +21,22 @@ class TestMutate(TestCase):
         # expected format for `prot_id_point_mutations_to_make` is:
         # {'SYUA_HUMAN': [{30: 'P'}], 'P10636-7': [{301: 'S'}, {301: 'L'}]}
         prot_id = 'test_peptide_AAP'
-        self.assertTrue(mutate._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
+        self.assertTrue(mutator._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
                                                               prot_id_point_mutations_to_make={prot_id: [{3: 'D'}]}))
-        self.assertTrue(mutate._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
+        self.assertTrue(mutator._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
                                                               prot_id_point_mutations_to_make={prot_id: [{3: 'D'},
                                                                                                          {2: 'E'}]}))
-        self.assertTrue(mutate._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
+        self.assertTrue(mutator._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
                                                               prot_id_point_mutations_to_make={prot_id: {3: 'D'}}))
 
-        self.assertFalse(mutate._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
+        self.assertFalse(mutator._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
                                                                prot_id_point_mutations_to_make={prot_id: {4: 'D'}}))
 
-        self.assertFalse(mutate._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
+        self.assertFalse(mutator._are_valid_positions_to_mutate(prot_id_seqs={prot_id: 'AAP'},
                                                                prot_id_point_mutations_to_make={prot_id: {0: 'D'}}))
 
-    def test__make_point_mutant_simple(self):
-        actual = mutate._make_point_mutants(prot_id_seq={'simple': 'AAP'}, point_mutants_to_make={3: ['D', 'T']})
+    def test__make_point_mutant_simple1(self):
+        actual = mutator._make_point_mutants(prot_id_seq={'simple': 'AAP'}, point_mutants_to_make={3: ['D', 'T']})
         expected = {'simple': 'AAP', 'simple(P3D)': 'AAD', 'simple(P3T)': 'AAT'}
         self.assertEqual(expected, actual)
 
@@ -45,22 +45,22 @@ class TestMutate(TestCase):
         # mock_get_sequences_by_uniprot_accession_nums_or_names.return_value =
         mock_get_sequences_by_uniprot_accession_nums_or_names.side_effect = [{'prot': 'AAP'}, {'prot2': 'KRI'}]
         prot_id_mutants_to_make = {'prot': {1: ['H'], 3: ['D', 'F']}, 'prot2': {2 : ['G']}}
-        actual = mutate.make_point_mutants(prot_id_mutants_to_make)
+        actual = mutator.make_point_mutants(prot_id_mutants_to_make)
         expected = {'prot': {'prot': 'AAP', 'prot(A1H)': 'HAP', 'prot(P3D)': 'AAD', 'prot(P3F)': 'AAF'},
                     'prot2':  {'prot2': 'KRI', 'prot2(R2G)': 'KGI'}}
         self.assertDictEqual(expected, actual)
 
     def test_mutate_protein(self):
-        actual = mutate.mutate(prot_seq='ACDEFG', pos_aa={4: 'Y', 6: 'Q'})
+        actual = mutator.mutate(prot_seq='ACDEFG', pos_aa={4: 'Y', 6: 'Q'})
         expected = 'ACDYFQ'
         self.assertEqual(expected, actual)
 
     def test_mutate_protein_invalid(self):
-        actual = mutate.mutate(prot_seq='ACDEFG', pos_aa={4: 'Y', 6: 'X'})
+        actual = mutator.mutate(prot_seq='ACDEFG', pos_aa={4: 'Y', 6: 'X'})
         expected = 'ACDYFG'
         self.assertEqual(expected, actual)
 
     def test_mutate_protein_invalid2(self):
-        actual = mutate.mutate(prot_seq='ACDEFG', pos_aa={4: 'Y', 7: 'Q'})
+        actual = mutator.mutate(prot_seq='ACDEFG', pos_aa={4: 'Y', 7: 'Q'})
         expected = 'ACDYFG'
         self.assertEqual(expected, actual)
