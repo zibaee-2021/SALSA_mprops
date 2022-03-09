1e18561b6626bd60e4b77b2269c59d37f4c7cdd4
zibaee-2021 <kdb5og@83mfol.site>
2022-03-09 13:13:27 +0000

    Transfer salsa-related code and combo-related code and lag-time prediction-related code out of mprops module into salsa module, new combo module and new predict_lag_time_hours script in order to adhere to 'separation of concerns'. Create new utils functionalities in plotter and utils.utils modules. Rename salsa.execute module as salsa.salsa.

---
+++ src/combo/mprops_bsc_combo.py
@@ -0,0 +1,111 @@
+from typing import Tuple
+from pandas import DataFrame as pDF
+from src.mean_properties import mprops
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from src.salsa import salsa
+from src.utils import plotter, utils
+"""
+The derivation of the coefficients for each of the 4 mean properties (mean beta-sheet propensity, mean hydrophilicity, 
+mean absolute net charge and mean total charge) is based on the kinetics of fibrillogenesis for recombinant 
+synuclein constructs measured in vitro. The calculations are similar to those described in Zibaee et al 2010 JBC
+but differ in some important details. 
+
+The combination of the 4 weighted properties into one is referred to as `mprops`. 
+The combination of normalised mprops and normalised SALSA b-strand contiguity integrals into one calculation is just 
+referred to as the `combined algorithm`. The use of the combined algorithm produces a fit to the lag times with an 
+Rsquared ("coefficient of determination") of ... (Rsquared of 1.0 is a perfect fit).
+
+Given the stochastic nature of fibrillogenesis, values are likely to be specific, not only to proteins that are 
+natively unfolded, and not only to synuclein-sized proteins, but also for the specific experimental conditions of the 
+assays as well. Hence I specify these conditions alongside the code and mathematical derivations: 
+Lag times of fibrillogensis of synuclein constructs was based on experiments under the following conditions: 
+- Protein concentration at 400 microMolars in 30 mM MOPs buffer pH 7.2 and 10 microMolar ThT. 
+- Incubation at 37degC, shaken at 450 rpm. 
+- 10 microlitre samples collect at 5 subsequent time points 4h, 8h, 24h, 48h, 96h. All measurements were performed on 
+each sample immediately after collection.
+Lag times were measured from each experiment, as time taken for ThT values to reach the square of their zero
+time emission readings at 480nm. The final value is the mean of the lag times for each constructs.
+
+A very important caveat is that for certain synuclein constructs for which lag time data was included, 
+which had longer lag times, those proteins did not begin to assemble within the 100 hours limit. Hence, 
+the data from these experiments was not included. As such their included data indicates these proteins to be more 
+fibrillogenic than they actually were taking in to account the non-inclusion of the aforementioned experiments. 
+"""
+
+
+def train_combo_model(syns_lags_seqs_props, make_plot: bool) -> Tuple[LinearRegression, float]:
+    """
+    Fit linear regression of given log of lag times to the given combination of physicochemical properties.    :param syns_lags_seqs_props:
+    :param make_plot: True to display 4 plots of the linear regression for each of the 4 mean properties
+    against the natural log of 'lag times'.
+    :return: Trained linear regression model of log of lag times against combined properties. Corresponding
+    coefficient of determination,
+    """
+    x_combo = np.array(syns_lags_seqs_props.combo)
+    x_combo = x_combo.reshape((-1, 1))
+    y = np.array(syns_lags_seqs_props.ln_lags)
+    model = LinearRegression()
+    model.fit(x_combo, y)
+    rsq = round(float(model.score(x_combo, y)), 3)
+    if make_plot: plotter.plot(model, x=x_combo, y=y, data_labels=list(syns_lags_seqs_props.index),
+                               title='combo', x_label='combined properties')
+    return model, rsq
+
+
+def _calc_relative_weights_for_nmprops_and_nbsc(syns_lags_seqs_props: pDF, make_plot: bool) -> Tuple[dict, dict, dict]:
+    """
+    Calculate the weights for the two given properties for the synucleins: normalised mprops (nmprops) and normalised
+    integrals of beta-strand contiguity (nbSC).
+    :param syns_lags_seqs_props:
+    :param make_plot: True to display plot.
+    :return: The weights for the nmprops and nbSC, as well as the intercepts and coefficients of determination for the
+    two linear regressions.
+    """
+    x_nbsc, x_nmprops = np.array(syns_lags_seqs_props.nbsc), np.array(syns_lags_seqs_props.nmprops)
+    x_nbsc, x_nmprops = x_nbsc.reshape((-1, 1)), x_nmprops.reshape((-1, 1))
+    y = np.array(syns_lags_seqs_props.ln_lags)
+    _2coefs, _2intcpts, _2rsq = {}, {}, {}
+    # NOTE: Pycharm bug `TypeError: 'NoneType' object is not callable` in debugger mode.
+    for prop_name, prop_values in zip(['nbsc', 'nmprops'], [x_nbsc, x_nmprops]):
+        model = LinearRegression()
+        model.fit(prop_values, y)
+        if make_plot: plotter.plot(model, x=prop_values, y=y, data_labels=list(syns_lags_seqs_props.index),
+                                   x_label=prop_name, title=prop_name)
+        _2coefs[prop_name] = round(float(model.coef_), 3)
+        _2intcpts[prop_name] = round(float(model.intercept_), 3)
+        _2rsq[prop_name] = round(float(model.score(prop_values, y)), 3)
+    print(f'_2rsq {_2rsq}')
+    return _2coefs, _2intcpts, _2rsq
+
+
+def generate_combo(syns_lnlags_seqs=None, make_plot: bool = True) -> pDF:
+    """
+    Predict lag time hours for all synucleins based on mean properties and beta-strand contiguity trained with
+    observed lag times. Optionally display plots for each of the properties.
+    :param syns_lnlags_seqs: Synuclein names, ln of lag times and sequences.
+    :param make_plot: True to display 4 plots of the linear regression for each of the 4 mean properties, for a plot
+    of mprops, a plot for beta-strand contiguity integrals and finally a plot of the combination of mprops and bSC.
+    All of the plots are against the natural log of 'lag times' on the y-axes.
+    :return: Synucleins mapped to predicted lag times in hours.
+    ['lag_time_means', 'ln_lags', 'seqs', 'mbp', 'mh', 'mnc', 'mtc', 'nmbp', 'nmh', 'nmnc', 'nmtc', 'mprops',
+    'nmprops', 'bsc', 'nbsc', 'combo', 'pred']
+    """
+    if syns_lnlags_seqs is None:
+        syns_lnlags_seqs = utils.get_ln_lags_and_build_seqs()
+    syns_lags_seqs_props = mprops.compute_norm_mprops(syns_lnlags_seqs, make_plot)
+    syns_lags_seqs_props = salsa.compute_norm_bsc_integrals(syns_lags_seqs_props)
+    _2coefs, _2intcpts, _2rsq = _calc_relative_weights_for_nmprops_and_nbsc(syns_lags_seqs_props, make_plot=make_plot)
+    syns_lags_seqs_props_ = syns_lags_seqs_props.copy()
+    syns_lags_seqs_props_.loc[:, 'combo'] = syns_lags_seqs_props.apply(lambda row: row.nbsc * _2coefs['nbsc'] +
+                                                               row.nmprops * _2coefs['nmprops'], axis=1)
+    return syns_lags_seqs_props_
+
+
+if __name__ == '__main__':
+    import time
+    st = time.time()
+    utils.get_ln_lags_and_build_seqs()
+    df = generate_combo(True)
+    print(f'time {round(time.time() - st, 2)} secs')
+    print(f'combo rsq {df[1]}')
